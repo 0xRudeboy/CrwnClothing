30c0ed789f7339018fe1cc45593343e7f0f606aa
0xRUDEBOY <116237237+0xRUDEBOY@users.noreply.github.com>
2023-05-24 10:49:12 -0400

    cart icon count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,14 +4,14 @@ import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
 
 const CartIcon = () => {
-  const { cartOpen, setCartOpen } = useContext(CartContext);
+  const { cartOpen, setCartOpen, cartCount } = useContext(CartContext);
 
   const toggleCartOpen = () => setCartOpen(!cartOpen);
 
   return (
     <div className="cart-icon-container" onClick={toggleCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
